11b2ca9c15d4a3583af37c6e76b943605d67a6e4
Xelzoid <zanggar.manarbek@icloud.com>
2024-09-06 18:07:37 +0500

    layout+minor things

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<li>
+  <a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/settings">Settings</a>
+</li>
+
+<style>
+  li {
+    list-style-type: none;
+    width: 100px;
+  }
+  li a {
+    display: block;
+    text-decoration: none;
+    margin-top: 15px;
+    padding: 35px 8px;
+    text-align: center;
+    background-color: #D9D9D9;
+    color: black;
+  }
+</style>
+<slot></slot>
