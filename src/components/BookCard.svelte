2e2bf9ee32c76517b6c8705ac003c81d43341d28
Medwon <sjajs.slqk1@gmail.com>
2024-09-06 19:52:02 +0500

    card

---
+++ src/components/BookCard.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let title = '';
+  export let author = '';
+  export let coverImage = '';
+</script>
+
+<div class="card_container">
+  <img src="{coverImage}" class="card_image" alt="Cover_Image">
+  <div>
+    <h2 class="title">{title}</h2>
+    <p class="author">{author}</p>
+  </div>
+</div>
+
+
+<style>
+  .card_container{
+    width: 200px;
+    height: 300px;
+    
+  }
+</style>
