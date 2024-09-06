53214937d85a8047b8e29a6abb3408a26245545f
Xelzoid <zanggar.manarbek@icloud.com>
2024-09-06 18:26:08 +0500

    layout update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,24 @@
+<nav>
+  <h1>LIBBON</h1>
+</nav>
 <li>
-  <a href="/">Home</a>
-	<a href="/about">About</a>
-	<a href="/settings">Settings</a>
+  <a href="/">home</a>
+	<a href="/about">profile</a>
+	<a href="/settings">social</a>
+  <a href="/settings">books</a>
+  <a href="/settings">games</a>
 </li>
+
 
 <style>
   li {
+    margin: 0;
     list-style-type: none;
-    width: 100px;
+    width: 7%;
+    height: 100%;
+    min-width: 100px;
+    padding-right: 10px;
+    border-right: 1px solid black;
   }
   li a {
     display: block;
@@ -17,6 +28,15 @@
     text-align: center;
     background-color: #D9D9D9;
     color: black;
+    border-radius: 15px;
+  }
+  nav {
+    border-bottom: 1px solid black;
+    width: 100%;
+  }
+  nav h1 {
+    text-align: center;
+    display: block;
   }
 </style>
 <slot></slot>
