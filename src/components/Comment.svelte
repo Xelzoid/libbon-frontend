dc3eafe2e4b1c598971d55d0d0158ab221820f20
Medwon <sjajs.slqk1@gmail.com>
2024-09-07 11:41:09 +0500

    comment_begining

---
+++ src/components/Comment.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let text = ""; 
+
+</script>
+
+<div class="container">
+    <textarea>{text}</textarea>
+</div>
+
+<style>
+    .container{
+        width: 600px;
+        border: none;
+        color: bisque;
+        font-size: 18px;
+    }
+</style>
