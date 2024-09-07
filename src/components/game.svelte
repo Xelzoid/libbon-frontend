8336f4640b77eb91e47dfd174629cbca22963804
Arsen1331 <arsen.tuyak01@gmail.com>
2024-09-07 22:15:48 +0600

    213

---
+++ src/components/game.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let name = "";
+    export let author = "";
+    export let description = "";
+    export let photo = "";
+</script>
+
+<div class="container">
+    <img class="book_img" src="{photo}">
+    <div class="content">
+        <h1>{name}</h1>
+        <h3>{author}</h3>
+        <p>{description}</p>
+    </div>
+</div>
+
+<style>
+    .container{
+        display: flex;
+        
+    }
+    .content{
+        margin-left: 15%;
+    }
+    .book_img{
+        height: 400px;
+        width: 300px;
+        object-fit: cover;
+    }
+    h1{
+        font-weight: bold;
+        font-size: 50px;
+    }
+    h3{
+        font-size: 30px;
+    }
+</style>
