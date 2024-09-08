f67a32a0b5c890bdaaae63b9b0d87c88e14c9783
Arsen1331 <arsen.tuyak01@gmail.com>
2024-09-08 10:36:54 +0600

    gh

---
+++ src/routes/games/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+import Gamecard from "../../components/Gamecard.svelte";
+import GameCard2 from "../../components/GameCard2.svelte";
+  let books = [
+    {
+      title: "Quiz",
+      author: "Викторина — игра, заключающаяся в ответах на устные или письменные вопросы",
+      coverImage: "",
+    },
+    {
+      title: "Wordle",
+      author: "Суть игры заключается в угадывании пятибуквенного слова за пяти попыток.",
+      coverImage: "",
+    }
+  ];
+</script>
+
+<div class="container">
+  <div class="card-container">
+    <!-- Карточка для первой книги -->
+    <Gamecard
+      title={books[0].title}
+      author={books[0].author}
+      coverImage={books[0].coverImage}
+    />
+
+    <!-- Карточка для второй книги -->
+    <GameCard2 
+      title={books[1].title}
+      author={books[1].author}
+      coverImage={books[1].coverImage}
+    />
+  </div>
+</div>
+
+<style>
+  .container {
+    margin-left: 10%;
+    margin-right: 5%;
+  }
+  .card-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 80px;
+  }
+</style>
