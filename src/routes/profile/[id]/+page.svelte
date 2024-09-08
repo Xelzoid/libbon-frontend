e1ea42c9b5a9a37b4e98165f220d6de11521465b
Arsen1331 <arsen.tuyak01@gmail.com>
2024-09-08 14:34:45 +0600

    123

--- src/routes/profile/[id]/+page.svelte
+++ src/routes/profile/[id]/+page.svelte
@@ -8,7 +8,6 @@
   import Button from "../../../components/Button.svelte";
   import ClubCard from "../../../components/CardClub.svelte";
   import Footer from "../../../components/Footer.svelte";
-  import { onMount } from "svelte";
   // import {FetchMe} from '$lib/utils'
   // @ts-ignore
   let user, friends = [], clubs = [], error = null, loading = true;
